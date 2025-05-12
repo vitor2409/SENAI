26491dedc42ffa549df06e11c77909a5d24c5611
vitor2409 <vitor_duarte1@estudante.sesisenai.org.br>
2025-05-12 14:02:00 +0000

    update

--- Lista 1/atividade7.py
+++ Lista 1/atividade7.py
@@ -1,2 +1,12 @@
 #Atividade 7
+moedas = {
+    0.01: int(input("Moedas de 1 centavo: ")),
+    0.05: int(input("Moedas de 5 centavos: ")),
+    0.10: int(input("Moedas de 10 centavos: ")),
+    0.25: int(input("Moedas de 25 centavos: ")),
+    0.50: int(input("Moedas de 50 centavos: ")),
+    1.00: int(input("Moedas de 1 real: "))
+}
+total = sum(valor * qtd for valor, qtd in moedas.items())
+print(f"Total economizado: R${total}")
 
